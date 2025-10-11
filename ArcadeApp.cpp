4056a97bdc7381fbc68e337fe03d534ec15e3e1f
Abhinav -WSS <141604493+Vyankesh-WSS@users.noreply.github.com>
2025-10-11 18:39:53 +0530

    push Screen Classes Change

--- ArcadeApp.cpp
+++ ArcadeApp.cpp
@@ -4,52 +4,23 @@
 #include "Utils/Vec2D.h"
 #include "Graphics/Color.h"
 #include "Graphics/ScreenBuffer.h"
+#include "Graphics/Screen.h"
 
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
+const int MAGNIFICATION = 3;
 
 using namespace std;
 
 
 int main(int argc, const char* argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO))
-    {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    Screen theScreen;
 
-    SDL_Window* optrWindow = SDL_CreateWindow(
-        "Arcade",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        0
-    );
-
-    if (!optrWindow)
-    {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    SDL_Surface* noptrWindowSurface = SDL_GetWindowSurface(optrWindow);
+    theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
+    theScreen.Draw(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, Color::Yellow());
+    theScreen.SwapScreens();
     
-    SDL_PixelFormat* pixelFormat = noptrWindowSurface->format;
-
-    Color::InitColorFormat(pixelFormat);
-
-    ScreenBuffer screenBuffer;
-
-    screenBuffer.Init(pixelFormat->format, noptrWindowSurface->w, noptrWindowSurface->h); // Createing msurface
-
-    screenBuffer.SetPixel(Color::Red(), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2); // Set Pixel center one
-
-    SDL_BlitSurface(screenBuffer.GetSurface(), nullptr, noptrWindowSurface, nullptr);
-   
-    SDL_UpdateWindowSurface(optrWindow);
-
     SDL_Event sdlEvent;
     bool running = true;
 
@@ -68,8 +39,7 @@ int main(int argc, const char* argv[])
 
     //SDL_Delay(2000); // Keep window open for 2 seconds
 
-    SDL_DestroyWindow(optrWindow);
-    SDL_Quit();
+ 
     return 0;
 }
 
