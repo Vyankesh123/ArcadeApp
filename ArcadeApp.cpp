4b116fd98c82fdc23093d8fd3de6964470572dea
Abhinav -WSS <141604493+Vyankesh-WSS@users.noreply.github.com>
2025-11-06 22:27:05 +0530

    Push Update(FrameRate)

--- ArcadeApp.cpp
+++ ArcadeApp.cpp
@@ -1,14 +1,7 @@
 #define SDL_MAIN_HANDLED
-#include "SDL.h"
+
 #include <iostream>
-#include "Utils/Vec2D.h"
-#include "Graphics/Color.h"
-#include "Graphics/ScreenBuffer.h"
-#include "Graphics/Screen.h"
-#include "Shapes/Line2D.h"
-#include "Triangle.h"
-#include "AARectangle.h"
-#include "Shapes/Circle.h"
+#include "App.h"
 
 
 const int SCREEN_WIDTH = 224;
@@ -20,41 +13,11 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    Screen theScreen;
-
-    theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
-    
-    Line2D line = { Vec2D(0,0),Vec2D(SCREEN_WIDTH,SCREEN_HEIGHT) };
-    Triangle triangle = { Vec2D(60,10),Vec2D(10,110),Vec2D(110,110) };
-    AARectangle rect = { Vec2D(SCREEN_WIDTH / 2 - 25,SCREEN_HEIGHT / 2 - 25),50,50 };
-    Circle circle = { Vec2D(SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2 + 50), 50 };
-
-    theScreen.Draw(triangle, Color::Red(),true, Color::Red());
-    theScreen.Draw(rect, Color::Blue(),true,Color::Blue());
-    theScreen.Draw(circle, Color(0,200,0,100), true, Color(0, 200, 0, 100));
-
-   // theScreen.Draw(line, Color::White());
-    //theScreen.Draw(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, Color::Yellow());
-    theScreen.SwapScreens();
-    
-    SDL_Event sdlEvent;
-    bool running = true;
-
-    while (running)
+    if (App::Singleton().Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION))
     {
-        while (SDL_PollEvent(&sdlEvent))
-        {
-            switch (sdlEvent.type)
-            {
-            case SDL_QUIT:
-                running = false;
-                break;
-            }
-        }
+        App::Singleton().Run();
     }
-
-    //SDL_Delay(2000); // Keep window open for 2 seconds
-
+    
  
     return 0;
 }
